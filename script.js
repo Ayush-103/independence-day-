2ca6d580f2eb5085bb14306762ebd8b71b4d7921
Ayush Tiwari <aidsews2304@glbitm.ac.in>
2024-08-14 23:58:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// script.js
+
+// Example of a simple script to alert users on page load
+window.onload = function() {
+    alert("Welcome to the Independence Day celebration in the Himalayas!");
+};
